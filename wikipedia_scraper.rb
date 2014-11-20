df76f62c1ff571f8e5914b35fe38e9de7198cfdc
Em01 <emma_williams1@hotmail.co.uk>
2014-11-20 14:33:09 +0000

    added text method

--- wikipedia_scraper.rb
+++ wikipedia_scraper.rb
@@ -4,4 +4,4 @@ require 'nokogiri'
 url = "http://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
 page = Nokogiri::HTML(open(url))
 
-puts page.css('td[style="text-align:left;"]')
+puts page.css('td[style="text-align:left;"]').text
